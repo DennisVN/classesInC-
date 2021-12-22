c83b27e8c6b409707f594eee1343214e2605c0ac
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-12-22 21:28:48 +0100

    Create Car class with static members

---
+++ Car.cs
@@ -0,0 +1,17 @@
+    class Car
+    {
+        string model;
+        // Make static so only class has acces and no 1 Object has ownership of it
+        public static int numberOfCars;
+        
+        public Car(String model)
+        {
+            this.model = model;
+            numberOfCars++;
+        }
+        // All cars need to start together right ? Static 
+        public static void StartRace()
+        {
+            Console.WriteLine("The race has begun ! ! ! ");
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -5,29 +5,37 @@ using Newtonsoft.Json.Linq;
 namespace ClassesExercise {
     class Program {
         static void Main(string[] args){
-
+            #region 
             // Create New Track Object (Instance of class) 
-            Track track1 = new Track();
-            Console.WriteLine(track1);
+            // Track track1 = new Track();
+            // Console.WriteLine(track1);
 
-            Track newTrack = new Track("Geen centen maar spullen", "Donnie ft. Frans Bauer");
-            Console.WriteLine(newTrack.Title + " - " + newTrack.Artist);
+            // Track newTrack = new Track("Geen centen maar spullen", "Donnie ft. Frans Bauer");
+            // Console.WriteLine(newTrack.Title + " - " + newTrack.Artist);
             
-            // Use parameters 
-            Track newTrack2 = new Track();
-            Console.WriteLine(newTrack2.Title + " - " + newTrack2.Artist);
+            // // Use parameters 
+            // Track newTrack2 = new Track();
+            // Console.WriteLine(newTrack2.Title + " - " + newTrack2.Artist);
 
-            Track newTrack3 = new Track();
-            newTrack3.Title = "Love is a battlefield";
-            newTrack3.Artist = "Kate Bush";
-            Console.WriteLine(newTrack3.Title + " - " + newTrack3.Artist);
+            // Track newTrack3 = new Track();
+            // newTrack3.Title = "Love is a battlefield";
+            // newTrack3.Artist = "Kate Bush";
+            // Console.WriteLine(newTrack3.Title + " - " + newTrack3.Artist);
 
-            Track newTrack4 = new Track();
-            newTrack4.Title = "Sigaretten";
-            Console.WriteLine(newTrack4.Title + " - " + newTrack4.Artist);
+            // Track newTrack4 = new Track();
+            // newTrack4.Title = "Sigaretten";
+            // Console.WriteLine(newTrack4.Title + " - " + newTrack4.Artist);
+            #endregion
+
+            Car car1 = new Car("Lambo");
+            Car car2 = new Car("Golfke");
+            Car car3 = new Car("Supra");
+            Console.WriteLine(Car.numberOfCars);
+            Car.StartRace();
 
             Console.ReadLine(); 
         }
 
     }
+
 }
